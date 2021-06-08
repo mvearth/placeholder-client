60e9e034180c90ff10250dc41510d323f9b7de40
Fábio Orlandi <36091563+fabioorlandi@users.noreply.github.com>
2021-06-07 21:14:17 -0300

    Added all fragments (#1)

---
+++ app/src/main/java/com/example/placeholder/ui/post/PostViewModel.java
@@ -0,0 +1,7 @@
+package com.example.placeholder.ui.post;
+
+import androidx.lifecycle.ViewModel;
+
+public class PostViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/placeholder/ui/suggestion/SuggestionViewModel.java
@@ -0,0 +1,7 @@
+package com.example.placeholder.ui.suggestion;
+
+import androidx.lifecycle.ViewModel;
+
+public class SuggestionViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
